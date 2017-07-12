fcd894ee6833eef37afef4aba87b57e2f84405ec
nazarenkod <nazarenkod1990@gmail.com>
2017-07-12 22:27:12 +0300

    Full project

--- src/test/java/Authorization.java
+++ src/test/java/Authorization.java
@@ -1,54 +1,48 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+
 /**
  * Created by nazarenkod on 12-Jul-17.
  */
 public class Authorization {
 
-// public WebDriver driver;
-//    public WebDriverWait wait;
 
     @Test(dataProvider="Authentication")
 
     public void app(String sUserName, String sPassword, String sModel, String sProxy)throws  Exception{
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--proxy-server=http://" + sProxy);
-        System.out.println(sProxy);
-//        System.setProperty("webdriver.chrome.driver", "E:\\Git\\test\\src\\main\\resources\\chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-        //    WebDriverWait wait = new WebDriverWait(driver,20);
-//        driver.get("");
-//        driver.findElement(By.id("")).clear();
-//        driver.findElement(By.id("")).sendKeys(sUserName);
-        System.out.println(sUserName);
-//        driver.findElement(By.id("")).clear();
-//        driver.findElement(By.id("")).sendKeys(sPassword);
-        System.out.println(sPassword);
-//        driver.findElement(By.id("")).click();
-        //wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id(""))));
-        //driver.get("url"+sModel);
-        System.out.println(sModel);
-        //Thread.sleep(500000);
-        //driver.get("url"+sModel);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--proxy-server=" + sProxy);
+        System.setProperty("webdriver.chrome.driver", "/home/nazarenkod/git/ChatProject/src/main/resources/driver/chromedriver");
+        WebDriver driver = new ChromeDriver(options);
 
-        //System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
-
-
-
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        driver.get("https://2ip.ru/");
+        Thread.sleep(10000);
+        driver.get("http://myfreecams.com/php/login.php?request=login&target");
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys(sUserName);
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys(sPassword);
+        driver.findElement(By.xpath("//input[@class='button']")).click();
+        Thread.sleep(Long.parseLong("5000"));
+        driver.get("http://www.myfreecams.com/#"+sModel);
     }
 
     @DataProvider
 
     public Object[][] Authentication() throws Exception{
-        Object[][] testObjArray = ExcelUtils.getTableArray("E:\\Git\\test\\testData\\TestData.xlsx","Sheet1");
+        Object[][] testObjArray = ExcelUtils.getTableArray("/home/nazarenkod/git/ChatProject/testData/TestData.xlsx","Sheet1");
         return (testObjArray);
 
     }
